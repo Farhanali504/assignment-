920ce1727a067d30d3e3bfddfb1ab58f534bfaf5
Farhan Ali Ashrafi <fa2822045@gmail.com>
2025-09-26 10:50:51 +0500

    Add files via upload

---
+++ assignment/app.js
@@ -0,0 +1,20 @@
+ 
+
+var userinput = prompt("Enter your roll number ");
+
+
+ if( ["2000","2003","2004","2005"].includes(userinput) ) {
+      console.log("present!");
+    
+} 
+else {
+  console.log("Absent");
+}
+
+//  if(x === "Vatican") {
+//     alert("Correct!");
+//  }
+//  else {
+//    alert("Wrong answer");
+//  }
+  
